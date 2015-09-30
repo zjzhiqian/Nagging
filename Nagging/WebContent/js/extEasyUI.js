93ba800522be5a90d6157975f32048c3f7d8327b
huangzhiqian <huangzhiqian@PC201503030847>
2015-09-30 17:46:31 +0800

    .

--- Nagging/WebContent/js/extEasyUI.js
+++ Nagging/WebContent/js/extEasyUI.js
@@ -41,15 +41,7 @@ $.extend($.fn.validatebox.defaults.rules, {
 });
 
 
-/**
- * @requires jQuery,EasyUI
- * 
- * 创建一个模式化的dialog
- * 
- * @returns $.modalDialog.handler 这个handler代表弹出的dialog句柄
- * 
- * @returns $.modalDialog.xxx 这个xxx是可以自己定义名称，主要用在弹窗关闭时，刷新某些对象的操作，可以将xxx这个对象预定义好
- */
+
 $.modalDialog = function(options) {
 	if ($.modalDialog.handler == undefined) {// 避免重复弹出
 		var opts = $.extend({
@@ -62,7 +54,7 @@ $.modalDialog = function(options) {
 				$(this).dialog('destroy');
 			}
 		}, options);
-		opts.modal = true;// 强制此dialog为模式化，无视传递过来的modal参数
+		opts.modal = true;
 		return $.modalDialog.handler = $('<div/>').dialog(opts);
 	}
 };
