147c1d15f97d67c4fd095f34fa07b6570738afe3
Administrator <Administrator@P8ZNUVOP3QNWGZI>
2015-10-18 22:28:52 +0800

    新增HttpClient的工具类以及Jar包

---
+++ Nagging/lucene/com/hzq/lucene/util/DataAnalyserUtil.java
@@ -0,0 +1,11 @@
+package com.hzq.lucene.util;
+
+
+/**
+ * 用来分析数据的工具类
+ * @author huangzhiqian
+ * @date 2015年10月18日 下午10:24:42
+ */
+public class DataAnalyserUtil {
+	
+}
--- Nagging/lucene/com/hzq/lucene/web/GrabController.java
+++ Nagging/lucene/com/hzq/lucene/web/GrabController.java
@@ -3,7 +3,15 @@ package com.hzq.lucene.web;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -14,7 +22,12 @@ import com.hzq.common.entity.Json;
 @Controller
 @RequestMapping("lucene")
 public class GrabController {
-	private static List<String> dataList;
+	public static List<String> dataList;
+	
+	public static void setDataList(List<String> dataList) {
+		GrabController.dataList = dataList;
+	}
+
 	private static boolean FinishFlag;
 	static {
 		dataList=Collections.synchronizedList(new ArrayList<String>());
@@ -35,7 +48,9 @@ public class GrabController {
 		}else{
 			j=new Json(true,"抓取开始");
 		}
-		new Thread(new DataGetTask()).start();
+		//TODO 尝试开启多线程进行抓取数据
+		ExecutorService service=Executors.newFixedThreadPool(1);
+		service.execute(new DataGetTask());
 		return j;
 	}
 	
@@ -58,6 +73,31 @@ public class GrabController {
 		public void run() {
 			for(int i=0;i<100000000;i++){
 				dataList.add("3");
+			}
+			//TODO 以实际数据抓取逻辑替换
+			try {
+				CloseableHttpClient httpclient = null;
+				CloseableHttpResponse response = null;
+				String url = "http://www.autohome.com.cn/ashx/series_allspec.ashx";
+				httpclient = HttpClients.custom().build();
+				try {
+						try {
+							HttpGet httpget = new HttpGet(url);
+							response = httpclient.execute(httpget);
+							HttpEntity entity = response.getEntity();
+							String content = EntityUtils.toString(entity);
+							System.err.println(content);
+							EntityUtils.consume(entity);
+						
+						} finally {
+							response.close();
+						}
+				} finally {
+					httpclient.close();
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+				throw new RuntimeException(e.getMessage());
 			}
 			FinishFlag=true;
 		}
