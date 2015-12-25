182fe0d21289d68979dc9ddbc860b39e319a74d8
huangzhiqian <huangzhiqian@PC201503030847>
2015-12-25 17:13:41 +0800

    `

--- Nagging/store/com/hzq/store/web/JavaScriptController.java
+++ Nagging/store/com/hzq/store/web/JavaScriptController.java
@@ -17,8 +17,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.hzq.system.entity.ShiroUser;
-
 /**
  * 
  * 
